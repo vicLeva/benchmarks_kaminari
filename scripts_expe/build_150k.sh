298cae1a2055a338e54d28556e34c279c60a9392
VicLeva <levallois.victor@outlook.fr>
2024-12-12 11:44:01 +0100

    150k

--- scripts_expe/build_150k.sh
+++ scripts_expe/build_150k.sh
@@ -8,6 +8,7 @@ index_dir="/WORKS/vlevallois/expes_kaminari/indexes"
 #constants
 tmp_dir="/WORKS/vlevallois/tmp"
 fof_salmonella_150k="/WORKS/vlevallois/data/dataset_pangenome_salmonella/fof_150k.list"
+fof_salmonella_kmindex_150k="/WORKS/vlevallois/data/dataset_pangenome_salmonella/fof_kmindex_150k.list"
 
 
 echo "!!!==!!! start cobs !!!==!!!" >> "$log_prefix/cobs/$log_suffix"
@@ -24,7 +25,7 @@ echo "!!!==!!! start fulgor !!!==!!!" >> "$log_prefix/fulgor/$log_suffix"
 
 echo "!!!==!!! start kmndex !!!==!!!" >> "$log_prefix/kmindex/$log_suffix"
 
-/usr/bin/time -v kmtricks pipeline --file "$fof_salmonella_150k" --run-dir "$index_dir"/kmindex/index_salmonella_150k_on_disk --kmer-size 25 --hard-min 1 --mode hash:bf:bin --bloom-size 36754250 -t 32 --until count >> "$log_prefix/kmindex/$log_suffix" 2>&1
+/usr/bin/time -v kmtricks pipeline --file "$fof_salmonella_kmindex_150k" --run-dir "$index_dir"/kmindex/index_salmonella_150k_on_disk --kmer-size 25 --hard-min 1 --mode hash:bf:bin --bloom-size 36754250 -t 32 --until count >> "$log_prefix/kmindex/$log_suffix" 2>&1
 #TODO : check bloom-size
 
 /usr/bin/time -v kmtricks merge --run-dir "$index_dir"/kmindex/index_salmonella_150k_on_disk --mode hash:bf:bin -t 4 >> "$log_prefix/kmindex/$log_suffix" 2>&1
@@ -43,3 +44,11 @@ rm -rf "$index_dir"/kmindex/index_salmonella_150k_on_disk/fpr \
 
 
 #===============================================================================
+
+echo "!!!==!!! start raptor !!!==!!!" >> "$log_prefix/raptor/$log_suffix"
+
+preprocessing_dir="/WORKS/vlevallois/expes_kaminari/indexes/raptor/preprocessing_files"
+
+/usr/bin/time -v raptor layout --input-file "$fof_salmonella_150k"  --kmer-size 31 --output-filename "$preprocessing_dir"/salmonella_150k_binning.out --threads 32 --false-positive-rate 0.05 --num-hash-functions 4 >> "$log_prefix/raptor/$log_suffix" 2>&1
+
+/usr/bin/time -v raptor build --input "$preprocessing_dir"/salmonella_150k_binning.out --output "$index_dir"/raptor/salmonella_150k.raptor --threads 32 >> "$log_prefix/raptor/$log_suffix" 2>&1
