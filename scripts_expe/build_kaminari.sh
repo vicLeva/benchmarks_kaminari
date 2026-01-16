cef1d77827bf52448742a92cb204b6c8b387af76
VicLeva <levallois.victor@outlook.fr>
2026-01-16 10:21:56 +0100

    update kam script

--- scripts_expe/build_kaminari.sh
+++ scripts_expe/build_kaminari.sh
@@ -5,7 +5,7 @@
 # =============================================================================
 
 # Executable command
-KAMINARI_CMD="/WORKS/vlevallois/softs/kaminari/build/kaminari"
+KAMINARI_CMD="/WORKS/vlevallois/softs/kaminari/build_on_disk/kaminari"
 
 # Paths
 LOG_DIR="/WORKS/vlevallois/expes_kaminari/logs/kaminari"
@@ -14,19 +14,20 @@ TMP_DIR="/WORKS/vlevallois/tmp"
 
 # Dataset FOFs (file of files): dataset_name=fof_path:is_metagenome
 declare -A DATASETS=(
-  [ecoli]="/WORKS/vlevallois/data/dataset_genome_ecoli/fof.list:false"
-  [human]="/WORKS/vlevallois/data/dataset_genome_human/fof.list:false"
-  [gut]="/WORKS/vlevallois/data/dataset_metagenome_gut/fof.list:true"
-  [salmonella]="/WORKS/vlevallois/data/dataset_pangenome_salmonella/fof.list:false"
-  [tara]="/WORKS/vlevallois/data/dataset_metagenome_tara/fof.list:true"
+  [ecoli]="/WORKS/vlevallois/data/dataset_genome_ecoli/fof.list"
+  [human]="/WORKS/vlevallois/data/dataset_genome_human/fof.list"
+  [gut]="/WORKS/vlevallois/data/dataset_metagenome_gut/fof.list"
+  [salmonella]="/WORKS/vlevallois/data/dataset_pangenome_salmonella/fof.list"
+  [tara]="/WORKS/vlevallois/data/dataset_metagenome_tara/fof.list"
+  [tara]="/WORKS/vlevallois/data/dataset_refseq/fof.list"
 )
 
 # Indexing parameters
 KMER_SIZE=31
 MINIMIZER_SIZE=19
-MEMORY_LIMIT=256
+MEMORY_LIMIT=262144 # 256GB
 THREADS=32
-VERBOSITY=3
+VERBOSITY=2
 
 # Log file setup
 mkdir -p "$LOG_DIR"
@@ -39,28 +40,21 @@ LOG_FILENAME="$LOG_DIR/build_$(date '+%Y-%m-%d_%H-%M-%S').log"
 echo "Starting Kaminari index construction..." | tee -a "$LOG_FILENAME"
 
 for dataset in "${!DATASETS[@]}"; do
-  IFS=":" read -r fof_path is_meta <<< "${DATASETS[$dataset]}"
-  output_index="$INDEX_DIR/${dataset}.kaminari"
+  fof_path="${DATASETS[$dataset]}"
+  output_index="$INDEX_DIR/${dataset}_index"
 
   echo "Building index for $dataset" | tee -a "$LOG_FILENAME"
-
-  # Optional metagenome flag
-  meta_flag=""
-  if [[ "$is_meta" == "true" ]]; then
-    meta_flag="--metagenome"
-  fi
 
   /usr/bin/time -v "$KAMINARI_CMD" build \
     -i "$fof_path" \
     -o "$output_index" \
     -k "$KMER_SIZE" \
     -m "$MINIMIZER_SIZE" \
-    -d "$TMP_DIR" \
     -a \
     -v "$VERBOSITY" \
     -t "$THREADS" \
     -g "$MEMORY_LIMIT" \
-    $meta_flag >> "$LOG_FILENAME" 2>&1
+     >> "$LOG_FILENAME" 2>&1
 
   echo "Completed $dataset" | tee -a "$LOG_FILENAME"
   echo "----------------------------------------------------" >> "$LOG_FILENAME"
